927e7f1ae4cbb694fe84b91cecc3066ab458f91c
cpzxo <acglove000@gmail.com>
2026-07-08 14:55:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 # 配置区域
 # ==============================================================================
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=6666666-6666-6666-6666-66666666666",
+    "https://host2play.gratis/server/renew?i=0fac8cba-9c75-4274-b6ad-ae3a9cde031e",
     # 添加更多链接
 ]
 
